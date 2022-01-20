e33b94cb08b6c80b0658982204a8b141372ea7f3
Timothy Karani <kartimothy@gmail.com>
2022-01-20 20:33:50 +0300

    Done.ish with received message

---
+++ src/components/ReceivedMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="basis-5/6 flex items-end mb-2">
+    <button class="btn btn-circle btn-md mr-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="inline-block w-6 h-6 stroke-current"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        ></path>
+      </svg>
+    </button>
+    <div class="card shadow-md bg-base-200">
+      <div class="card-body">
+        <p>
+          Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit
+          sit necessitatibus veritatis sed molestiae voluptates incidunt iure
+          sapiente.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReceivedMessage",
+};
+</script>
+
+<style></style>
