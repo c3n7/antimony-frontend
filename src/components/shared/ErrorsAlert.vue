b4979323faac552e3638347854799307c051fef3
Timothy Karani <kartimothy@gmail.com>
2022-01-21 20:53:52 +0300

    Can show error alerts

---
+++ src/components/shared/ErrorsAlert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-full">
+    <div class="mt-5 w-full" v-for="error in errors" :key="error.key">
+      <div class="alert alert-error flex-row">
+        <div class="flex-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="w-6 h-6 mx-2 stroke-current flex-none"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+            ></path>
+          </svg>
+
+          <label class="text-left flex-1">
+            <h4 class="capitalize">{{ error.key }}</h4>
+            <p class="text-sm text-base-content text-opacity-60">
+              {{ error.value }}
+            </p>
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorAlerts",
+  props: {
+    errors: Array,
+  },
+};
+</script>
+
+<style></style>
--- src/pages/SignInView.vue
+++ src/pages/SignInView.vue
@@ -3,65 +3,70 @@
     class="w-screen h-screen bg-base-200 bg-opacity-80 p-2 flex items-center justify-center"
   >
     <div
-      class="card shadow bg-base-200 basis-full md:basis-7/12 lg:basis-6/12 xl:basis-4/12 2xl:basis-3/12"
+      class="basis-full md:basis-7/12 lg:basis-6/12 xl:basis-4/12 2xl:basis-3/12"
     >
-      <div class="card-body">
-        <h2 class="card-title">Sign In to Antimony</h2>
+      <div class="card shadow bg-base-200 w-full">
+        <div class="card-body">
+          <h2 class="card-title">Sign In to Antimony</h2>
 
-        <div class="space-y-1">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email@host.com"
-              class="input input-bordered"
-              :class="{ 'input-error input-bordered': emailValidation }"
-              v-model="email"
-            />
-            <label class="label" v-if="emailValidation">
-              <span class="label-text-alt text-error">{{
-                emailValidation
-              }}</span>
-            </label>
-          </div>
+          <div class="space-y-1">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email@host.com"
+                class="input input-bordered"
+                :class="{ 'input-error input-bordered': emailValidation }"
+                v-model="email"
+              />
+              <label class="label" v-if="emailValidation">
+                <span class="label-text-alt text-error">{{
+                  emailValidation
+                }}</span>
+              </label>
+            </div>
 
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              class="input input-bordered"
-              :class="{ 'input-error input-bordered': passwordValidation }"
-              v-model="password"
-            />
-            <label class="label" v-if="emailValidation">
-              <span class="label-text-alt text-error">{{
-                emailValidation
-              }}</span>
-            </label>
-          </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                class="input input-bordered"
+                :class="{ 'input-error input-bordered': passwordValidation }"
+                v-model="password"
+              />
+              <label class="label" v-if="emailValidation">
+                <span class="label-text-alt text-error">{{
+                  emailValidation
+                }}</span>
+              </label>
+            </div>
 
-          <div class="pt-3">
-            <button class="btn btn-block btn-primary" @click="signIn()">
-              Sign In
-            </button>
-          </div>
+            <div class="pt-3">
+              <button class="btn btn-block btn-primary" @click="signIn()">
+                Sign In
+              </button>
+            </div>
 
-          <div class="link link-hover link-primary w-full text-center pt-3">
-            Register
+            <div class="link link-hover link-primary w-full text-center pt-3">
+              Register
+            </div>
           </div>
         </div>
       </div>
+
+      <ErrorsAlert :errors="errors" />
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
+import ErrorsAlert from "../components/shared/ErrorsAlert.vue";
 export default {
   name: "SignInView",
   computed: {
@@ -84,10 +89,10 @@ export default {
       if (this.emailValidation !== "" || this.passwordValidation !== "") {
         return;
       }
-
       this.authenticate({ email: this.email, password: this.password });
     },
   },
+  components: { ErrorsAlert },
 };
 </script>
 
