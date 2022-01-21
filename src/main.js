eac80f148272d8af667c2f14589bcb0f07667237
Timothy Karani <kartimothy@gmail.com>
2022-01-21 20:05:19 +0300

    Added cookie setting function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles.css";
+
 import router from "./utils/router";
 import state from "./utils/state";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000/api/v1";
 
 createApp(App).use(router).use(state).mount("#app");
