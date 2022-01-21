4045fc37799b138cd67e0022f3c4e3459ef55c21
Timothy Karani <kartimothy@gmail.com>
2022-01-21 17:21:33 +0300

    Can change theme from local storage

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@
 </template>
 
 <script>
+import { onMounted } from "vue";
+
 export default {
   name: "App",
+  setup() {
+    onMounted(() => {
+      console.log("Mounted");
+
+      window.addEventListener("storage", (e) => {
+        // When local storage changes, dump the list to
+        // the console.
+        console.log(e);
+        if (e.newValue) {
+        }
+        document.documentElement.setAttribute("data-theme", e.newValue);
+        console.log(window.localStorage.getItem("theme"));
+      });
+    });
+  },
 };
 </script>
 
