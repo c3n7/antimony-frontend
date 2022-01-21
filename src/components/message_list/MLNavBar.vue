a07adb294863ff9ce7b63cb5a506ccbcfdb98518
Timothy Karani <kartimothy@gmail.com>
2022-01-21 17:57:47 +0300

    Can change theme from dropdown

--- src/components/message_list/MLNavBar.vue
+++ src/components/message_list/MLNavBar.vue
@@ -1,12 +1,35 @@
 <template>
-  <div
-    class="navbar mb-2 shadow-lg bg-neutral text-neutral-content fixed top-0 left-0 right-0 z-10"
-  >
+  <div class="navbar mb-2 shadow-lg bg-neutral fixed top-0 left-0 right-0 z-10">
     <div class="flex-1 px-2 mx-2">
-      <span class="text-lg font-bold"> Antimony Chat </span>
+      <span class="text-lg font-bold text-neutral-content">
+        Antimony Chat
+      </span>
     </div>
-    <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
+    <div class="flex-none space-x-1">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" class="m-1 btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+            />
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-auto max-h-72 overflow-y-scroll"
+        >
+          <li v-for="daisyTheme in daisyThemes" :key="daisyTheme">
+            <a @click="changeTheme(daisyTheme)">{{ daisyTheme }}</a>
+          </li>
+        </ul>
+      </div>
+      <button class="btn btn-square btn-ghost text-neutral-content">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -28,6 +51,41 @@
 <script>
 export default {
   name: "MLNavBar",
+  data: function () {
+    return {
+      daisyThemes: [
+        "light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk",
+      ],
+    };
+  },
+  methods: {
+    changeTheme(theme) {
+      console.log("Changing to " + theme);
+      window.localStorage.setItem("theme", theme);
+      document.documentElement.setAttribute("data-theme", theme);
+    },
+  },
 };
 </script>
 
