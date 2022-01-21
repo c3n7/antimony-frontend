6696bf5af7b7d8a9f3a7c169fa19f01649395303
Timothy Karani <kartimothy@gmail.com>
2022-01-21 16:51:22 +0300

    Done with base layout of item

---
+++ src/components/message_list/MLItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-center mb-2 justify-start basis-full space-x-1">
+    <button class="btn btn-circle w-14 h-14">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+
+    <div class="flex-1 md:basis-10/12 px-3 py-4 overflow-hidden">
+      <div class="text-md font-bold">Jean Dean</div>
+      <div class="truncate opacity-60 text-sm">
+        Voluptatum assumenda quasi amet harum cupiditate. Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Quibusdam dicta laboriosam placeat
+        illo. Nemo laudantium pariatur expedita quia, est veniam similique iure
+        explicabo magni saepe repellendus illo nihil id fugit.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MLItem",
+};
+</script>
+
+<style></style>
--- src/pages/MessageListView.vue
+++ src/pages/MessageListView.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="relative">
+  <div class="relative min-h-screen bg-base-200 bg-opacity-80">
     <MLNavBar />
+    <div class="pt-20 px-2">
+      <MLItem />
+    </div>
   </div>
 </template>
 
 <script>
 import MLNavBar from "../components/message_list/MLNavBar.vue";
+import MLItem from "../components/message_list/MLItem.vue";
 export default {
   name: "MessageListView",
-  components: { MLNavBar },
+  components: { MLNavBar, MLItem },
 };
 </script>
 
