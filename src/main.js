ed0115af41f43bebf61cc902ef45ded1c7004110
Timothy Karani <kartimothy@gmail.com>
2022-01-31 19:52:48 +0300

    Changed to heroku backend url

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from "./utils/router";
 import state from "./utils/state";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
+// axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "https://antimony-backend.herokuapp.com/api/v1";
 
 createApp(App).use(router).use(state).mount("#app");
